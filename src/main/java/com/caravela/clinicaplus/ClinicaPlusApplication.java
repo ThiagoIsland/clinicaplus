4328763b7edf37dc6b74620340f97f32f4545823
Julio Dias <julio.dias@logone.com.br>
2025-07-28 21:41:31 -0300

    first commit

---
+++ src/main/java/com/caravela/clinicaplus/ClinicaPlusApplication.java
@@ -0,0 +1,13 @@
+package com.caravela.clinicaplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caravela/clinicaplus/ClinicaPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caravela.clinicaplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
